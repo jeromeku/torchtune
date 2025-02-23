af48935427872595089f4aeb4cc751b0d10999a2
jeromeku <jerome.ku@gmail.com>
2025-02-22 18:22:50 -0800

    add op schema name

--- my-tests/simple_tensor_subclass.py
+++ my-tests/simple_tensor_subclass.py
@@ -1,5 +1,9 @@
 import torch
+from torch._ops import OpOverload
 from torch.testing._internal.common_fsdp import FSDPTest
+from torchao.dtypes.nf4tensor import NF4Tensor, to_nf4
+
+from torchtune.modules.low_precision import FrozenNF4Linear
 
 
 class SimpleTensor(torch.Tensor):
@@ -33,14 +37,28 @@ class SimpleTensor(torch.Tensor):
             return func(mat1, simple_tensor.inner_tensor, bias)
         try:
             print(f"calling {func.__name__} with args: {[type(arg) for arg in args]} and kwargs: {kwargs}")
-            #with torch._C.DisableTorchFunctionSubclass():
-            return func(*args, **kwargs)
+            with torch._C.DisableTorchFunctionSubclass():
+                return func(*args, **kwargs)
         except Exception:
             print(f"ERR: subclass doesn't implement {func}")
 
-    def __torch_dispatch__(self, func, types, args=(), kwargs=None):
-        print(f"dispatching {func.name} with args: {[type(arg) for arg in args]} and kwargs: {kwargs}")
+    def __torch_dispatch__(self, func: OpOverload, types, args=(), kwargs=None):
+        
+        print(f"dispatching {func._schema.name} {func._opname} {func._overloadname} with {len(args)} args: {[type(arg) for arg in args]} and kwargs: {kwargs}")
+        if func is torch.ops.aten.detach.default:
+            print(f"returning {args[0]}")
+            return args[0]
         return func(*args, **kwargs)
+
+class SimpleLinear(torch.nn.Linear):
+    def __init__(self, in_features, out_features):
+        super().__init__(in_features, out_features, bias=False)
+        self.weight.requires_grad_(False)
+        simple_tensor = SimpleTensor(self.weight)
+        torch.utils.swap_tensors(self.weight, torch.nn.Parameter(simple_tensor, requires_grad=False))
+
+    def forward(self, x):
+        return torch.nn.functional.linear(x, self.weight)
 
 dtype = torch.bfloat16
 device = "cuda"
@@ -48,6 +66,8 @@ batch_size = 2
 in_features = 256
 out_features = 128
 inner_tensor = torch.randn(out_features, in_features, dtype=dtype, device=device)
-weight = SimpleTensor(inner_tensor)
-activation = torch.randn(batch_size, in_features, dtype=dtype, device=device)
-out = torch.nn.functional.linear(activation, weight)out = torch.nn.functional.linear(activation, weight)
+model = SimpleLinear(in_features, out_features)
+# x = torch.randn(batch_size, in_features, dtype=dtype, device=device)
+# out = model(x)
+# print(out)
+
