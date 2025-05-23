a97b1fbe67c520c154c379b6e77028fb5b09263e
jeromeku <jerome.ku@gmail.com>
2025-05-23 14:54:59 +0000

    add programmatic launch

---
+++ scripts/run_lora.py
@@ -0,0 +1,26 @@
+import argparse
+import runpy
+import sys
+from pathlib import Path
+from unittest.mock import patch
+
+TUNE_PATH = "torchtune/_cli/tune.py"
+
+
+def main(args):
+    cmd = f"""
+    tune run --nnodes {args.nnodes} --nproc_per_node {args.ngpus} {args.recipe} --config {args.config} \
+    """
+    print(f"{cmd.strip()}")
+    with patch.object(sys, "argv", cmd.split()):
+        runpy.run_path(TUNE_PATH, run_name="__main__")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--nnodes", type=int, default=1)
+    parser.add_argument("--ngpus", type=int, default=1)
+    parser.add_argument("--recipe", type=str, default="my_recipes/lora_finetune_distributed.py")
+    parser.add_argument("--config", type=str, default="my_configs/qwen3_1.7B_lora.yaml")
+    args = parser.parse_args()
+    main(args)
+    
